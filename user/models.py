5fb5ee0b36ec73863316863e843fd5f3f52f4889
Hyun Suk <cs982606@gmail.com>
2020-12-14 18:13:00 +0900

    MODIFY: user, app추가

---
+++ user/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class User(models.Model):
+    email    = models.EmailField(max_length=45)
+    password = models.CharField(max_length=200)
+    sex      = models.CharField(max_length=20)
+    birthday = models.DateField
+    country  = models.ForeignKey('country',through='Country')
+
+    class Meta:
+        db_table='users'
+
+class Country(models.Model):
+    name_kor = models.CharField(max_length=45)
+    name_eng = models.CharField(max_length=45)
+
+    class Meta:
+        db_table='countries'
